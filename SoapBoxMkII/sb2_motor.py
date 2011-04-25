89940cd8e588ee8ec0ff2a9c1aaf6e8a4bdd08dd
Vasco Nevoa <vasco.nevoa@sapo.pt>
2011-04-25 01:30:13 +0100

    Real implementation for the I2C power bridge.

--- SoapBoxMkII/sb2_motor.py
+++ SoapBoxMkII/sb2_motor.py
@@ -17,25 +17,65 @@
 #    along with DiffTrike. If not, see <http://www.gnu.org/licenses/>.
 
 #
-# This module implements the electric motor power driver.
-# Currently it is a dummy that just lights up LEDs.
+# This module implements the electric motor driver and data extraction.
 #
+
+import fcntl, struct
+import time
+
+IOCTL_I2C_SETSLAVEADDRESS = 0x0703
+
+# Bridge controller registers:
+REG_COMMAND = 0
+REG_STATUS = 1
+REG_PWM = 2
+REG_TEMPERATURE = 4
+REG_CURRENT = 5
+REG_BAT_VOLTAGE = 6
+REG_FW_VERSION = 7
 
 class I2CMotorBridge():
 
-	def __init__(self, filename):
-		self.desiredTorque = 0
-		self.file = open(filename,'w', 0)
+	def __init__(self, filename, address):
+		self.file = open(filename,'r+b', 0)
+		fcntl.ioctl(self.file, IOCTL_I2C_SETSLAVEADDRESS, address)
 
 	def __del__(self):
-		close(self.file)
+		self.file.close()
 
-	def setTorque(self, desired):
-		self.desiredTorque = desired
-		self.file.write(str(int(desired*255)))
+	def getRawData(self):
+		self.file.write(struct.pack('B', 0)) # seek to beginning of registers.
+		return struct.unpack('BBBBBBBB', self.file.read(8)) # dump all of them.
 
-	def getTorque(self):
-		return self.desiredTorque
+	def getCurrent(self):
+		self.file.write(struct.pack('B', REG_CURRENT))
+		(T,) = struct.unpack('B', self.file.read(1))
+		return (T / 10.0) # Ampere
 
-	def getSpeed(self):
-		return 0
+	def getTemperature(self):
+		self.file.write(struct.pack('B', REG_TEMPERATURE))
+		(t,) = struct.unpack('B', self.file.read(1))
+		return t # Celsius
+
+	def getVoltage(self):
+		self.file.write(struct.pack('B', REG_BAT_VOLTAGE))
+		(v,) = struct.unpack('B', self.file.read(1))
+		return ((v + 100) / 10.0) # Volt
+
+	def setTorque(self, desired): # [0..1]
+		self.file.write(struct.pack('BB', REG_PWM, int(desired*255))) # PWM duty cycle, 0-255
+
+# This is a simple test routine that only runs if this module is 
+# called directly with "python sb2_motor.py"
+
+if __name__ == '__main__':
+	m = I2CMotorBridge('/dev/i2c-0', 0x22)
+	for x in range(3):
+		t0 = time.time()
+		m.file.write(struct.pack('B', REG_BAT_VOLTAGE))
+		t1 = time.time()
+		m.file.read(1)
+		t2 = time.time()
+		print " Write=%0.1fms;  Read=%0.1fms" % (1000*(t1-t0), 1000*(t2-t1))
+
+
