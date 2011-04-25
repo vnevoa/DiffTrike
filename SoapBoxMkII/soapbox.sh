7714c0d28bb4ec3a796c8594cdb37909092df558
Vasco Nevoa <vasco.nevoa@sapo.pt>
2011-04-25 10:47:34 +0100

    Added accelerometer tuning.

--- SoapBoxMkII/soapbox.sh
+++ SoapBoxMkII/soapbox.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 set -e
 
-# /etc/init.d/soapbox: start and stop the "SoapBoxMkII control" daemon
+# /etc/init.d/soapbox: start and stop the MyOwnHybrid "SoapBoxMkII control" daemon
 
 APP=/home/root/sb2.py
 BT=/sys/class/rfkill/rfkill0/state
 WI=/sys/class/rfkill/rfkill1/state
 USBHOST=/sys/devices/platform/s3c2410-ohci/usb_mode
+ACC1=/sys/devices/platform/spi_s3c24xx_gpio.0/spi3.0
+ACC2=/sys/devices/platform/spi_s3c24xx_gpio.0/spi3.1
 
 test -x $APP || exit 0
 
@@ -20,11 +22,15 @@ case "$1" in
 	# disable Bluetooth transmitter:
 	echo 0 > $BT # bluetooth off
 
-	# enable USB Host mode (unpowered):
+	# enable USB Host mode:
 	ifconfig usb0 down
 	echo host > $USBHOST
 	modprobe usbhid
 	modprobe joydev
+
+	# tune accelerometers:
+	echo 50 > $ACC1/threshold
+	echo 50 > $ACC2/threshold
 
 	# launch control application:
 	export SDL_JOYSTICK_DEVICE=/dev/input/js2
@@ -42,4 +48,3 @@ case "$1" in
 esac
 
 exit 0
-
