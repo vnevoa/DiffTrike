c67c5680a1ebcc8b3fd10b942f51e1cfdf4436fa
Vasco Nevoa <vasco.nevoa@sapo.pt>
2011-07-13 10:10:42 +0100

    Added GPS and PMU start-up configurations.

--- SoapBoxMkII/soapbox.sh
+++ SoapBoxMkII/soapbox.sh
@@ -7,8 +7,10 @@ APP=/home/root/sb2.py
 BT=/sys/class/rfkill/rfkill0/state
 WI=/sys/class/rfkill/rfkill1/state
 USBHOST=/sys/devices/platform/s3c2410-ohci/usb_mode
-ACC1=/sys/devices/platform/spi_s3c24xx_gpio.0/spi3.0
-ACC2=/sys/devices/platform/spi_s3c24xx_gpio.0/spi3.1
+ACC1=/sys/devices/platform/s3c2440-i2c/i2c-0/0-0073/lis302dl.1
+ACC2=/sys/devices/platform/s3c2440-i2c/i2c-0/0-0073/lis302dl.2
+#ACC1=/sys/devices/platform/spi_s3c24xx_gpio.0/spi3.0
+#ACC2=/sys/devices/platform/spi_s3c24xx_gpio.0/spi3.1
 GPS=/sys/devices/platform/gta02-pm-gps.0/power_on
 
 test -x $APP || exit 0
@@ -17,6 +19,7 @@ case "$1" in
   start)
 
 	echo -n "Starting SoapBoxMkII control application... "
+	echo "1" > /sys/devices/platform/leds-gpio/leds/gta02:red:aux/brightness
 
 	# network interfaces are auto configured and pulled up by 'ifup'.
 
@@ -25,6 +28,8 @@ case "$1" in
 
 	# turn on GPS receiver:
 	echo 1 > $GPS
+	sleep 1
+	cat /home/root/gps/*.ubx > /dev/ttySAC1
 
 	# enable USB Host mode:
 	ifconfig usb0 down
@@ -36,6 +41,9 @@ case "$1" in
 	echo 50 > $ACC1/threshold
 	echo 50 > $ACC2/threshold
 
+	# quiet down the I2C bus:
+	/home/root/i2c_silence.py
+
 	# launch control application:
 	export SDL_JOYSTICK_DEVICE=/dev/input/js2
 	start-stop-daemon -N -10 -S -x $APP > /dev/null &
@@ -45,6 +53,7 @@ case "$1" in
   stop)
         echo "Stopping SoapBoxMkII control application."
 	start-stop-daemon -K -x /usr/bin/python $APP
+	echo "0" > /sys/devices/platform/leds-gpio/leds/gta02:red:aux/brightness
 	;;
   *)
 	echo "Usage: /etc/init.d/soapbox {start|stop}"
