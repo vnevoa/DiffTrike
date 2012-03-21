4fa8c2a98f40f27db0e25aaa5d53be551e1dd4ab
Vasco Nevoa <vasco.nevoa@sapo.pt>
2012-03-21 23:58:44 +0000

    Added a few fields, distinguished reals and integers, and new Logging functions.

--- SoapBoxMkII/sb2_output.py
+++ SoapBoxMkII/sb2_output.py
@@ -20,22 +20,24 @@
 # and the telemetry module.
 #
 
-import struct, random
+import struct, random, time
 
 class outputData():
 	def __init__(self):
 
 		""" Holds all the necessary system output data """
-
-		self.l_trq = 0 # Left wheel desired torque.
-		self.r_trq = 0 # Right wheel desired torque.
-		self.t_in = 0  # Time taken during inputs.
-		self.t_proc = 0 # Time taken during processing.
-		self.t_out = 0  # Time taken during outputs.
-		self.t_cycl = 0 # Period of loop cycle.
+		self.tstamp = 0.0 # time stamp.
+		self.l_trq = 0.0 # Left wheel desired torque.
+		self.r_trq = 0.0 # Right wheel desired torque.
+		self.t_in = 0.0  # Time taken during inputs.
+		self.t_proc = 0.0 # Time taken during processing.
+		self.t_out = 0.0  # Time taken during outputs.
+		self.t_cycl = 0.0 # Period of loop cycle.
 		self.failed = False # Generic Fail Flag for Output.
 		self.failed_r = False # Fail Flag for Right Motor Output.
 		self.failed_l = False # Fail Flag for Left Motor Output.
+		self.glitches_r = 0 # right bridge I2C glitches.
+		self.glitches_l = 0 # left bridge I2C glitches.
 		self.seed = 0 # only for random test data.
 
 	def randomize(self):
@@ -43,39 +45,62 @@ class outputData():
 
 		#self.seed = ( random.random() + self.seed ) / 2
 		self.seed = random.random()
-
 		self.l_trq = self.seed * 2 - 1 # -1..1 
 		self.r_trq = 1 - self.seed * 2 # -1..1
-		self.t_in = 0  # 
-		self.t_proc = 0 # 
-		self.t_out = 0  # 
-		self.t_cycl = 0 # 
+		self.t_in = 0.0  # 
+		self.t_proc = 0.0 # 
+		self.t_out = 0.0  # 
+		self.t_cycl = 0.0 # 
+		self.glitches_r += 1
+		self.glitches_l += 1
 
 	def serialize(self):
 
 		""" grabs all the data fields and stuffs them into a string for network communications """
 
-		return struct.pack("ffffffbbb",
-		self.t_in, self.t_proc,	self.t_out, self.t_cycl,
+		self.tstamp = time.time()
+		return struct.pack("dffffff???ii",
+		self.tstamp, self.t_in, self.t_proc, self.t_out, self.t_cycl,
 		self.l_trq, self.r_trq,
-		self.failed, self.failed_r, self.failed_l)
+		self.failed, self.failed_r, self.failed_l,
+		self.glitches_r, self.glitches_l)
 
 
 	def deserialize(self, stream):
 
 		""" grabs a binary string and explodes it into all the data fields """
 
-		(self.t_in, self.t_proc, self.t_out, self.t_cycl,
+		(self.tstamp, self.t_in, self.t_proc, self.t_out, self.t_cycl,
 		self.l_trq, self.r_trq,
-		self.failed, self.failed_r, self.failed_l) = \
-		struct.unpack("ffffffbbb", stream)
+		self.failed, self.failed_r, self.failed_l,
+		self.glitches_r, self.glitches_l) = \
+		struct.unpack("dffffff???ii", stream)
+
+	def log(self):
+
+		""" grabs all the data fields and returns them in a string """
+
+		return "{0:.3f}".format(self.tstamp) + ";" + "{0:.2f}".format(self.t_in) + ";" + "{0:.2f}".format(self.t_proc) + ";" + "{0:.2f}".format(self.t_out) + ";" + \
+		"{0:.2f}".format(self.l_trq) + ";" + "{0:.2f}".format(self.r_trq) + ";" + \
+		"{0:d}".format(self.failed) + ";" + "{0:d}".format(self.failed_r) + ";" + "{0:d}".format(self.failed_l) + ";" + \
+		"{0:d}".format(self.glitches_r) + ";" + "{0:d}".format(self.glitches_l) + ";"
+		
+	def logHeader(self):
+
+		""" returns the names of all the data fields """
+
+		return "tstamp;t_in;t_proc;t_out;l_trq;r_trq;failed;failed_r;failed_l;glitches_r;glitches_l;"
+
 
 # This is a simple test routine that only runs if this module is 
 # called directly with "python sb2_output.py"
 
 if __name__ == '__main__':
-	i = outputData()
-	a = i.serialize()
+	o = outputData()
+	a = o.serialize()
 	print len(a)
-	i.deserialize(a)
+	o.deserialize(a)
+	print o.logHeader()
+	print o.log()
+
 
