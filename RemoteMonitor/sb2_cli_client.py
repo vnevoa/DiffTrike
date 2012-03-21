598fe28181ec4ee4b69dfb519adcc3b771018da5
Vasco Nevoa <vasco.nevoa@sapo.pt>
2012-03-21 23:56:07 +0000

    Reimplemented generically based on Input and Output classes.

--- RemoteMonitor/sb2_cli_client.py
+++ RemoteMonitor/sb2_cli_client.py
@@ -23,42 +23,19 @@
 # controller in real time on the console.
 #
 
-import socket, sys, time
+import sys, time
 sys.path.append("../SoapBoxMkII")
-import sb2_input, sb2_output
+import sb2_input, sb2_output, sb2_gui_data
 
-HOST = "192.168.5.202"
-PORT = 11000
-data = " ".join(sys.argv[1:])
-
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-
-# Receive data from the server and shut down
-t_1 = time.time()
-i = sb2_input.inputData()
-o = sb2_output.outputData()
-bi = len(i.serialize())
-bo = len(o.serialize())
-print "input data is %d bytes, output data is %d bytes, total is %d." % (bi, bo, bi+bo)
-print
-while True:
-	received = sock.recv(bi+bo)
-	#print "received = %d" % len(received)
-	if (len(received) == bi+bo):
-		#print len(received), print len(received[0:bi]), len(received[bi:bi+bo])
-		i.deserialize(received[0:bi])
-		o.deserialize(received[bi:bi+bo])
-		t = time.time()
-		print "ACC=%0.3f, %0.3f" % (i.accX, i.accY)
-		print "GPS=%d, %0.1f, %0.1f" % (i.gpsVld, i.gpsSpd, i.gpsHdng)
-		#print "T:%0.3f: L:%d C:%0.3f I:%0.3f P:%0.3f O:%0.3f X:%0.3f Y:%0.3f R:%0.3f L:%0.3f" % \
-		#( (t - t_1), len(received), t_cycl, t_in, t_proc, t_out, X, Y, right, left )
-		t_1 = t
-	else:
-		print "failed reception (%d bytes)" % len(received)
-
-sock.close()
+tele = sb2_gui_data.Telemetry("192.168.5.202", 11000)
+print ""
+print "Inputs: ", tele.i.logHeader(), "\n"
+print "Outputs: ", tele.o.logHeader(), "\n"
+time.sleep(0.5)
+while tele.connected:
+	print "Inputs: ", tele.i.log()
+	print "Outputs: ", tele.o.log()
+	print ""
+	time.sleep(0.5)
 
 
